34c9fd6fab14349fc1da8fd2ed13839f1049b6bb
RaviUsdad <web.developerravi8@gmail.com>
2022-12-07 23:38:17 +0530

    header

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@
       clickable: true,
     },
   });
-})(jQuery);   
+})(jQuery);
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
@@ -43,26 +43,66 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 /* FadeIn Scroll */
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
   /* Every time the window is scrolled ... */
-  $(window).scroll( function(){
-  
-      /* Check the location of each desired element */
-      $('.fade').each( function(i){
-          
-          var bottom_of_object = $(this).position().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height();
-          
-          /* If the object is completely visible in the window, fade it it */
-          if( bottom_of_window > bottom_of_object ){
-              
-              $(this).animate({'opacity':'1'},900);
-                  
-          }
-          
-      }); 
-  
+  $(window).scroll(function () {
+
+    /* Check the location of each desired element */
+    $('.fade').each(function (i) {
+
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+
+        $(this).animate({ 'opacity': '1' }, 900);
+
+      }
+
+    });
+
   });
-  
+
 });
+
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", () => {
+  navbar.classList.toggle("showInput");
+  if (navbar.classList.contains("showInput")) {
+    searchBox.classList.replace("bx-search", "bx-x");
+  } else {
+    searchBox.classList.replace("bx-x", "bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function () {
+  navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function () {
+  navLinks.classList.toggle("show3");
+}
